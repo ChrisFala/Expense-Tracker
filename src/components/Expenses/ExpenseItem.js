ae3144dfbf1a9fc9edbff7f337271358c8418811
ChrisFala <christopherfala42@gmail.com>
2022-10-11 12:43:27 -0700

    NewExpense Form and Styling. Tracking form field states

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,16 +4,16 @@ import "./ExpenseItem.css";
 import { useState } from 'react';
 
 const ExpenseItem = (props) => {
-    
-    const [title, setTitle] = useState(props.title, );
-    
+
+    const [title, setTitle] = useState(props.title,);
+
     const clickHandler = () => {
         setTitle('Updated!');
     };
-    
+
     return (
         <Card className="expense-item">
-            <ExpenseDate date={props.date}/>
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
