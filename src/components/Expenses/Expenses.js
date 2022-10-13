7961a1613fc1318aad39d7dd6c4a21d840ac0ca5
ChrisFala <christopherfala42@gmail.com>
2022-10-13 15:26:15 -0700

    Dynamic Expense Items Display

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,35 +6,22 @@ import { useState } from 'react';
 
 const Expenses = (props) => {
   const [enteredYear, setEnteredYear] = useState(new Date().getFullYear())
-  
+
   const selectYearHandler = (enteredYearData) => {
-    setEnteredYear(enteredYearData); 
+    setEnteredYear(enteredYearData);
   }
-  
+
   return (
     <div>
       <Card className="expenses">
-      <ExpensesFilter selectedYear={enteredYear} onSelectYear={selectYearHandler}/>
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        <ExpensesFilter selectedYear={enteredYear} onSelectYear={selectYearHandler} />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
